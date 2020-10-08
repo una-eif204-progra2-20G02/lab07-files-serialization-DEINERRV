0d14be80b97c329be27d4ce7a688725291752d93
Deiner <deiner.ra10@gmail.com>
2020-10-07 16:36:14 -0800

    se agrego una funcion virtual pura, save(vector<Persona>)

--- src/ISave.h
+++ src/ISave.h
@@ -1,11 +1,14 @@
 #ifndef MY_PROJECT_NAME_ISAVE_H
 #define MY_PROJECT_NAME_ISAVE_H
 #include "Persona.h"
+#include<vector>
 
 
 class ISave {
 public:
     virtual void save(Persona*)=0;
+    virtual void save(std::vector<Persona>)=0;
+
     ~ISave();
 };
 
