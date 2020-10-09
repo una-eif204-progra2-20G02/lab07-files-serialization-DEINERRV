92ca80f4ba3b2b0f89a28f4d8519a436b4049233
Deiner <deiner.ra10@gmail.com>
2020-10-08 23:29:54 -0800

    se implemento la interfaz IRead

--- src/JSON.cpp
+++ src/JSON.cpp
@@ -38,14 +38,53 @@ void JSON::save(std::vector<Persona> personas) {
         exit(1);
     }
 
-    json Serializacion;
+    vector<json> Serializacion;
 
     for (int i=0;i<personas.size();i++){
-        Serializacion["Personas"] += {personas[i].getNombre(),personas[i].getEdad(),personas[i].getId()};
+
+        json PersonaJSON;
+        PersonaJSON["nombre"]=personas[i].getNombre();
+        PersonaJSON["edad"]=personas[i].getEdad();
+        PersonaJSON["id"]=personas[i].getId();
+
+        Serializacion.push_back(PersonaJSON);
     }
 
-    string personaSerializada=Serializacion.dump(4);
-    archivo << personaSerializada << '\n';
+    archivo << Serializacion << '\n';
 
     archivo.close();
+}
+
+
+void from_json(const json& j, Persona& sb) {
+    sb.setNombre(j.at("nombre"));
+    sb.setEdad(j.at("edad"));
+    sb.setId(j.at("id"));
+}
+
+
+void from_json(const json& j, vector<Persona>& s) {
+    const json& sj = j;
+    s.resize(sj.size());
+    std::copy(sj.begin(), sj.end(), s.begin());
+}
+
+std::vector<Persona> JSON::read() {
+
+    ifstream archivo;
+
+    try { archivo.open("ArchivoJSON.json", ios::binary); }
+
+    catch (ifstream::failure a) {
+        cout << "no se pudo abrir el archivo";
+        exit(1);
+    }
+
+    json j;
+    archivo>>j;
+
+    archivo.close();
+
+    vector<Persona> x = j;
+    return x;
 }
--- src/JSON.h
+++ src/JSON.h
@@ -1,11 +1,13 @@
 #ifndef MY_PROJECT_NAME_JSON_H
 #define MY_PROJECT_NAME_JSON_H
 #include "ISave.h"
+#include "IRead.h"
 
-class JSON: public ISave {
+class JSON: public ISave,public IRead{
 public:
     void save(Persona *) override;
     void save(std::vector<Persona>)override;
+    std::vector<Persona> read() override;
 };
 
 
